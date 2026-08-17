5d1eb3bc6ab669d3b31d0df71946f1de79e5a663
Elsa <01primex.eu@gmail.com>
2026-08-17 13:33:42 +0200

    Ignore Next.js development build output

--- .next-dev/static/chunks/app_furniture_page_tsx_91a63b1d._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_65e9bb1c._.js",
-  "static/chunks/node_modules_lucide-react_dist_esm_icons_9e8d3d43._.js"
-],
-    source: "dynamic"
-});
